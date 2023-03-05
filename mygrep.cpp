00c8f2c5974e6a45b23bd653d6583c5faec4a056
Joose Saksanen <joose.saksanen@gmail.com>
2023-03-05 14:27:57 +0200

    second increment done

--- mygrep.cpp
+++ mygrep.cpp
@@ -1,43 +1,80 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 bool belongsTo(string temp, string search, int &position);
+void simplePrint(string temp, string search, bool found, int &pos);
 
-int main () {
+int main (int argc,char* argv[]) {
 	string templateString;
 	string searchString;
 	bool includes;
 	int position;
-	cout << "Give me a string from which to search for: ";
-	getline(cin, templateString);
-	//cin.ignore();
-	cout << "Give serach string: ";
-	getline(cin, searchString);
-	//cout << templateString << endl;
-	//cout << searchString[2] << endl;
+	int counter;
+	if (argc == 1) {
+		cout << "Give me a string from which to search for: ";
+		getline(cin, templateString);
+		cout << "Give serach string: ";
+		getline(cin, searchString);
+		includes = belongsTo(templateString, searchString, position);
+		simplePrint(templateString, searchString, includes, position);
 
-	includes = belongsTo(templateString, searchString, position);
-	if (includes) {
-
-		cout << "\"" << searchString << "\"" << " found in " << "\"" << templateString << "\"" << " in position " << position << endl;
-	} else {
-		cout << "\"" << searchString << "\"" << " NOT found in " << "\"" << templateString << "\"" << endl;
 	}
+	if (argc == 2) {
+		cout << "Invalid number of arguments!" << endl;
+	}
+	if (argc == 3) {
+		searchString = argv[1];
+		ifstream textFile;
+		string line;
+		textFile.open(argv[2]);
+		while(getline(textFile, line)) {
+			includes = belongsTo(line, searchString, position);
+			if (includes == true) {
+				cout << line << endl;
+			}
+		}
 
+	}
+	/*if (argc == 4) {
+		searchString = argv[2];
+		ifstream textFile;
+		string line;
+		textFile.open(argv[3]);
+		string argument = argv[1];
+		if (argument == "-olo") {
+			//cout << "-olo" << endl;
+
+		} if (argument == "")
+		while(getline(textFile, line)) {
+			includes = belongsTo(line, searchString, position);
+			if (includes == true) {
+				cout << line << endl;
+			}
+		}
+	}
+	*/
 	return 0;
 }
 
 bool belongsTo(string temp, string search, int &position) {
 	size_t found = temp.find(search);
-	//cout << found << endl;
-	if (found > 100) {
-
+	if (found > 100) {	//if find function doesn't find correct value, it returns a very big number
 		return false;
 	} else {
 		position = found;
 		return true;
 	}
 }
-//string.find()
+
+void simplePrint(string temp, string search, bool found, int &pos) {
+	if (found) {
+		cout << "\"" << search << "\"" << " found in " << "\"" << temp << "\"" << " in position " << pos << endl;
+	} else {
+		cout << "\"" << search << "\"" << " NOT found in " << "\"" << temp << "\"" << endl;
+	}
+
+}
+
